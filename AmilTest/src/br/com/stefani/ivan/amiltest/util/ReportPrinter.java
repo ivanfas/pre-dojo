70cb8bd2dbeb68cfd5e8bb03aacc1fbaf3a80965
ivanfas <ivanfas@gmail.com>
2015-01-09 16:20:08 -0200

    Versao 1.0.

--- AmilTest/src/br/com/stefani/ivan/amiltest/util/ReportPrinter.java
+++ AmilTest/src/br/com/stefani/ivan/amiltest/util/ReportPrinter.java
@@ -1,7 +1,10 @@
 package br.com.stefani.ivan.amiltest.util;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
+import br.com.stefani.ivan.amiltest.model.Death;
 import br.com.stefani.ivan.amiltest.model.Match;
 import br.com.stefani.ivan.amiltest.model.Player;
 
@@ -17,11 +20,32 @@ public class ReportPrinter {
 		for (Match match : this.matchList) {
 			System.out.println("------ Partida: " + match.getIdMatch() );
 			for (Player player : match.getPlayerMap().values()) {
-				System.out.println("--------- Jogador: " + player.getIdStrPlayer() );
-				System.out.println("------------ Assassinatos: " + player.getKillList().size() );
-				System.out.println("------------ Mortes: " + player.getDeathList().size() );
+				if( !player.getIdStrPlayer().toUpperCase().contains("WORLD") ){
+					System.out.println("--------- Jogador: " + player.getIdStrPlayer() );
+					System.out.println("------------ Assassinatos: " + player.getKillList().size() );
+					System.out.println("------------ Mortes: " + player.getDeathList().size() );
+					
+					//Armas usadas
+					LinkedHashMap<EnumDeathType, Integer> prefDeathType = new LinkedHashMap<EnumDeathType, Integer>();
+					for(Death kill: player.getKillList()){
+						 if( prefDeathType.containsKey(kill.getDeathType()) ){
+							 Integer temp = prefDeathType.get(kill.getDeathType());
+							 temp++;
+						 } else {
+							 prefDeathType.put(kill.getDeathType(), 1);
+						 }
+					}
+					if( prefDeathType.size() > 0 ){
+						System.out.println("------------ Armas Utilizadas: " );
+						for(Entry<EnumDeathType, Integer> deathType : prefDeathType.entrySet()){
+							System.out.println("--------------- " + deathType.getKey() + ": " +deathType.getValue() );
+						}
+					}
+					
+					
+				}
+
 			}
 		}
 	}
-
 }
